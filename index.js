625b29688e3f04f9358f6d3fae6f7a1618c981b8
Enayet <enayetflweb@gamil.com>
2023-12-04 15:33:05 +0600

    mongodb connection security update, try catch block added to routes, cors added

--- index.js
+++ index.js
@@ -10,8 +10,24 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
+// Specify the allowed origins
+const allowedOrigins = ['http://localhost:5173', 'https://yourdomain.com'];
 
-const uri = "mongodb+srv://mongoose-test:jQT7HZ8p0jtT7TD8@cluster0.ktgpsav.mongodb.net/?retryWrites=true&w=majority";
+// CHANGE THE ABOVE SECOND DOMAIN TO THE REAL DOMAIN OF THE CUSTOMER.
+
+// CORS configuration with allowed origins
+app.use(cors({
+  origin: function (origin, callback) {
+    // Check if the request origin is in the allowedOrigins array or if it's undefined (for non-browser requests)
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ktgpsav.mongodb.net/?retryWrites=true&w=majority`;
 // CHANGE USER ID AND PASSWORD IN THE ABOVE URI
 
 mongoose.connect(uri, {
@@ -22,9 +38,38 @@ mongoose.connect(uri, {
 
 const connection = mongoose.connection;
 
+// Handle MongoDB connection errors
+connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err.message);
+  // You can customize the error handling here, such as logging, sending alerts, etc.
+  // For now, let's exit the process to prevent the application from continuing with a broken connection.
+  process.exit(1);
+});
+
+// Handle MongoDB disconnection
+connection.on('disconnected', () => {
+  console.warn('MongoDB disconnected. Reconnecting...');
+  // Attempt reconnection after a short delay
+  setTimeout(() => {
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: 'mongoosecom'  // CHANGE THE DB NAME HERE
+    });
+  }, 5000); // Adjust the delay as needed
+});
+
+
 connection.once('open', () => {
   console.log('MongoDB database connection established successfully')
 })
+
+process.on('SIGINT', () => {
+  connection.close(() => {
+    console.log('MongoDB connection closed due to application termination');
+    process.exit(0);
+  });
+});
 
 
 const productSchema = new mongoose.Schema({
@@ -91,65 +136,110 @@ const Order = mongoose.model('Order', orderSchema);
 // FOR ALL PRODUCTS
 
 app.get('/api/v1/allproducts', async (req, res) => {
-  const result = await Product.find();
-  res.send(result);
+  try{
+    const result = await Product.find();
+    res.send(result);
+  } catch(error){
+    console.error('Error fetching produts:', error.message);
+    res.status(500).send("Internal Server Error")
+  }
 });
 
 
 // EDIT PRODUCT GET ROUTE
 app.get('/api/v1/allproducts/:id', async (req, res) => {
-  const id = req.params.id;
+  try{
+    const id = req.params.id;
   const result = await Product.findById(id);
   res.send(result);
+  } catch (error){
+    console.error('Error fetching products:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // ALL ORDERS GET ROUTE
 app.get('/api/v1/allorders', async (req, res) => {
+ try{
   const result = await Order.find();
   res.send(result);
+ } catch(error){
+  console.error('Error fetching products:', error.message);
+  res.status(500).send('Internal Server Error');
+ }
 });
 
 // EDIT ORDER GET ROUTE
 app.get('/api/v1/allorders/:id', async (req, res) => {
-  const id = req.params.id;
+  try{
+    const id = req.params.id;
   const result = await Order.findById(id);
   res.send(result);
+  } catch(error){
+    console.error('Error fetching products:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // ALL BLOGS GET ROUTE
 app.get('/api/v1/allblogs', async (req, res) => {
+ try{
   const result = await Blog.find();
   res.send(result);
+ } catch (error){
+  console.error('Error fetching products:', error.message);
+    res.status(500).send('Internal Server Error');
+ }
 });
 
 // EDIT BLOG GET ROUTE
 app.get('/api/v1/allblogs/:id', async (req, res) => {
-  const id = req.params.id;
+  try{
+    const id = req.params.id;
   const result = await Blog.findById(id);
   res.send(result);
+  } catch(error){
+    console.error('Error fetching products:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // POST ROUTES ------------
 
 // FOR ADD PRODUCT POST ROUTE
 app.post('/api/v1/addproduct', async (req, res) => {
-  const product = new Product(req.body);
+  try{
+    const product = new Product(req.body);
   const result = await product.save();
   res.send(result);
+  } catch (error){
+    console.error('Error adding product:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // FOR ADD ORDER POST ROUTE
 app.post('/api/v1/order', async (req, res) => {
-  const order = new Order(req.body);
+  try{
+    const order = new Order(req.body);
   const result = await order.save();
   res.send(result);
+  } catch (error){
+    console.error('Error adding product:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // FOR ADD BLOG POST ROUTE
 app.post('/api/v1/addblog', async (req, res) => {
-  const blog = new Blog(req.body);
+  try{
+    const blog = new Blog(req.body);
   const result = await blog.save();
   res.send(result);
+  } catch(error){
+    console.error('Error adding product:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // PATCH ROUTES ------------
@@ -158,8 +248,13 @@ app.post('/api/v1/addblog', async (req, res) => {
 app.patch('/api/v1/order/:id', async (req, res) => {
   const id = req.params.id;
   const updatedOrderData = req.body;
-  const result = await Order.findByIdAndUpdate(id, { $set: { status: updatedOrderData.status } }, { new: true });
-  res.send(result);
+  try{
+    const result = await Order.findByIdAndUpdate(id, { $set: { status: updatedOrderData.status } }, { new: true });
+    res.send(result);
+  } catch (error){
+    console.error('Error updating order:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // EDIT PRODUCT PATCH ROUTE
@@ -174,8 +269,14 @@ app.patch('/api/v1/allproducts/:id', async (req, res) => {
 app.patch('/api/v1/allblogs/:id', async (req, res) => {
   const id = req.params.id;
   const updatedBlog = req.body;
+ try{
   const result = await Blog.findByIdAndUpdate(id, { $set: updatedBlog }, { new: true });
   res.send(result);
+ }
+ catch(error){
+  console.error('Error deleting product:', error.message);
+  res.status(500).send('Internal Server Error');
+ }
 });
 
 // DELETE ROUTES ------------
@@ -183,185 +284,36 @@ app.patch('/api/v1/allblogs/:id', async (req, res) => {
 // FOR DELETE PRODUCT ROUTE
 app.delete('/api/v1/allproduct/:id', async (req, res) => {
   const id = req.params.id;
-  const result = await Product.findByIdAndDelete(id);
-  res.send(result);
+ try{ const result = await Product.findByIdAndDelete(id);
+  res.send(result);}
+  catch(error){
+    console.error('Error deleting product:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // FOR DELETE BLOG ROUTE
 app.delete('/api/v1/allblogs/:id', async (req, res) => {
   const id = req.params.id;
-  const result = await Blog.findByIdAndDelete(id);
+  try{
+    const result = await Blog.findByIdAndDelete(id);
   res.send(result);
+  }
+  catch (error){
+    console.error('Error deleting product:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-// async function run() {
-//   try {
-//     // await client.connect();
-//     // await client.db('admin').command({ ping: 1 });
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
-//     const productCollection = client.db('mishbir').collection('products');
-//     const orderCollection = client.db('mishbir').collection('orders');
-//     const blogCollection = client.db('mishbir').collection('blogs');
-
-//     // GET ROUTE------------
-
-//     // FOR ALL PRODUCTS
-
-//     app.get('/api/v1/allproducts', async (req, res) => {
-//       const result = await productCollection.find().toArray();
-//       res.send(result);
-//     });
-
-//     // EDIT PRODUCT GET ROUTE
-
-//     app.get('/api/v1/allproducts/:id', async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const result = await productCollection.findOne(query);
-//       res.send(result);
-//     });
-
-//     // ALL ORDER GET ROUTE
-
-//     app.get('/api/v1/allorders', async (req, res) => {
-//       const result = await orderCollection.find().toArray();
-//       res.send(result);
-//     });
-
-//     // EDIT ORDER GET ROUTE
-
-//     app.get('/api/v1/allorders/:id', async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const result = await orderCollection.findOne(query);
-//       res.send(result);
-//     });
-
-//     // ALL BLOGS GET ROUTE
-
-//     app.get('/api/v1/allblogs', async (req, res) => {
-//       const result = await blogCollection.find().toArray();
-//       res.send(result);
-//     });
-
-//     //  EDIT BLOG GET ROUTE
-
-//     app.get('/api/v1/allblogs/:id', async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const result = await blogCollection.findOne(query);
-//       res.send(result);
-//     });
-
-//     //  POST ROUTE-----------
-
-//     // FOR ADD PRODUCT POST ROUTE
-//     app.post('/api/v1/addproduct', async (req, res) => {
-//       const product = req.body;
-//       console.log(product);
-//       const result = await productCollection.insertOne(product);
-//       res.send(result);
-//     });
-
-//     //  FOR ADD ORDER POST ROUTE
-//     app.post('/api/v1/order', async (req, res) => {
-//       const order = req.body;
-//       const result = await orderCollection.insertOne(order);
-//       res.send(result);
-//     });
-
-//     // FOR ADD BLOG POST ROUTE
-//     app.post('/api/v1/addblog', async (req, res) => {
-//       const blog = req.body;
-//       const result = await blogCollection.insertOne(blog);
-//       res.send(result);
-//     });
-
-//     // PATCH ROUTE--------------
-
-//     // EDIT ORDER PATCH ROUTE
-//     app.patch('/api/v1/order/:id', async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const updatedOrderData = req.body;
-//       const updateDoc = {
-//         $set: {
-//           status: updatedOrderData.status,
-//         },
-//       };
-//       const result = await orderCollection.updateOne(query, updateDoc);
-//       res.send(result);
-//     });
-
-//     // EDIT PRODUCT PATCH ROUTE
-//     app.patch('/api/v1/allproducts/:id', async (req, res) => {
-//       const id = req.params.id;
-//       const updatedProductData = req.body;
-//       const query = { _id: new ObjectId(id) };
-//       const updateDoc = {
-//         $set: {
-//           category: updatedProductData.category,
-//           color: updatedProductData.color,
-//           desc: updatedProductData.desc,
-//           featured_image: updatedProductData.featured_image,
-//           gallery_image: updatedProductData.gallery_image,
-//           size: updatedProductData.size,
-//           title: updatedProductData.title,
-//           rprice: updatedProductData.rprice,
-//           sprice: updatedProductData.sprice,
-//         },
-//       };
-//       const result = await productCollection.updateOne(query, updateDoc);
-//       res.send(result);
-//     });
-
-//     // EDIT BLOG PATCH ROUTE
-
-//     app.patch('/api/v1/allblogs/:id', async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const updatedBlog = req.body;
-//       const updateDoc = {
-//         $set: {
-//           blogTitle: updatedBlog.blogTitle,
-//           blog: updatedBlog.blog,
-//           featured_image: updatedBlog.featured_image,
-//         },
-//       };
-//       const result = await blogCollection.updateOne(query, updateDoc);
-//       res.send(result);
-//     });
-
-//     // DELETE ROUTE ------------------------
-
-//     // FOR DELETE PRODUCT ROUTE
-//     app.delete('/api/v1/allproduct/:id', async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const result = await productCollection.deleteOne(query);
-//       res.send(result);
-//     });
-
-//     // FOR DELETE BLOG ROUTE
-
-//     app.delete('/api/v1/allblogs/:id', async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const result = await blogCollection.deleteOne(query);
-//       res.send(result);
-//     });
-
-//     // console.log(
-//     //   'Pinged your deployment. You successfully connected to MongoDB!'
-//     // );
-//   } finally {
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('mishbir server is running');
+  res.send('ECOMMERCE server is running');
 });
 
 app.listen(port, () => {
